b3ebb555f6977756e24e761479b231530c87c291
Jonas <Jonas@VAIO-Jonas>
2015-05-24 14:19:06 +0200

    Standard Gui

---
+++ Game/src/com/game/client/GuiIntro.java
@@ -0,0 +1,17 @@
+package com.game.client;
+
+public class GuiIntro extends Gui {
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
