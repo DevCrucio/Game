f7cd4034e8bc3a6d47c4359a158828dac55bce8f
Jonas <Jonas@VAIO-Jonas>
2015-05-24 14:16:08 +0200

    Base Gui

---
+++ Game/src/com/game/client/Gui.java
@@ -0,0 +1,15 @@
+package com.game.client;
+
+public abstract class Gui {
+	public abstract void update(float delta);
+
+	public abstract void render();
+
+	public void create() {
+
+	}
+
+	public void destroy() {
+
+	}
+}
