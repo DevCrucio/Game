53a9ab47187df0114db1c4cc2e3293d655796d70
Jonas <Jonas@VAIO-Jonas>
2015-05-24 13:45:30 +0200

    Logging

--- Game/src/com/game/util/Misc.java
+++ Game/src/com/game/util/Misc.java
@@ -1,7 +1,25 @@
 package com.game.util;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
+import com.esotericsoftware.kryo.Kryo;
+
 public class Misc {
+	/*
+	 * For Logging
+	 */
+	public static Date date = new Date();
+
 	public static void log(String msg) {
-		System.out.println("[Info] " + msg);
+		System.out.println("[Info " + new Timestamp(date.getTime()) + "] "
+				+ msg);
+	}
+
+	/*
+	 * Method that registers on Client and Server the same way
+	 */
+	public static void reg(Kryo kryo) {
+
 	}
 }
