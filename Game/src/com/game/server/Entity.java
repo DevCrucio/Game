753ebcbd6a959be62e0f259ae0725520aa6219c3
Jonas <Jonas@VAIO-Jonas>
2015-05-24 21:30:08 +0200

    Base Entity

---
+++ Game/src/com/game/server/Entity.java
@@ -0,0 +1,16 @@
+package com.game.server;
+
+public abstract class Entity {
+	public final int ID;
+	public final World world;
+	public float x, y, dx, dy;
+
+	public Entity(World world, int ID) {
+		this.ID = ID;
+		this.world = world;
+	}
+
+	public abstract void update(float delta);
+
+	public abstract void render();
+}
