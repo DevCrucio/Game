3bc975599870d26559d052b1332e010deb648454
Jonas <Jonas@VAIO-Jonas>
2015-05-24 17:47:43 +0200

    Basic Player Rendering

--- Game/src/com/game/client/EntityPlayer.java
+++ Game/src/com/game/client/EntityPlayer.java
@@ -1,15 +1,41 @@
 package com.game.client;
 
-public abstract class EntityPlayer extends Entity {
+import org.lwjgl.opengl.GL11;
+
+public class EntityPlayer extends Entity {
 
 	public EntityPlayer(World world, int ID) {
 		super(world, ID);
 	}
 
-	public abstract void poll(float delta);
+	public void poll(float delta) {
 
-	public abstract void cam();
+	}
 
-	public abstract void gui();
+	public void cam() {
+
+		GL11.glTranslatef(480 - x, 270 - y, 0);
+	}
+
+	public void gui() {
+
+	}
+
+	@Override
+	public void update(float delta) {
+
+	}
+
+	@Override
+	public void render() {
+		GL11.glTranslatef(x, y, 0);
+		GL11.glColor4f(0.8f, 0.4f, 0.2f, 1.0f);
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glVertex2f(-5, -5);
+		GL11.glVertex2f(5, -5);
+		GL11.glVertex2f(5, 5);
+		GL11.glVertex2f(-5, 5);
+		GL11.glEnd();
+	}
 
 }
