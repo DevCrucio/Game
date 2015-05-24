734c4bf40992a861589f01c1e011e6ed07601380
Jonas <Jonas@VAIO-Jonas>
2015-05-24 18:15:19 +0200

    Entity Player moveable

--- Game/src/com/game/client/EntityPlayer.java
+++ Game/src/com/game/client/EntityPlayer.java
@@ -1,5 +1,6 @@
 package com.game.client;
 
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 
 public class EntityPlayer extends Entity {
@@ -9,11 +10,32 @@ public class EntityPlayer extends Entity {
 	}
 
 	public void poll(float delta) {
-
+		// UP DOWN
+		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+			dy = 1f;
+		} else if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+			dy = -1f;
+		} else {
+			dy = 0;
+		}
+		// LEFT RIGHT
+		if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+			dx = 1f;
+		} else if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+			dx = -1f;
+		} else {
+			dx = 0;
+		}
+		// Speed
+		float speed = .1f;
+		float current = (float) Math.sqrt(dx * dx + dy * dy);
+		if (current > speed) {
+			dx = dx / current * speed;
+			dy = dy / current * speed;
+		}
 	}
 
 	public void cam() {
-
 		GL11.glTranslatef(480 - x, 270 - y, 0);
 	}
 
@@ -23,11 +45,14 @@ public class EntityPlayer extends Entity {
 
 	@Override
 	public void update(float delta) {
-
+		// TODO: Collision Detection
+		x += dx * delta;
+		y += dy * delta;
 	}
 
 	@Override
 	public void render() {
+		// Base Quad Rendering
 		GL11.glTranslatef(x, y, 0);
 		GL11.glColor4f(0.8f, 0.4f, 0.2f, 1.0f);
 		GL11.glBegin(GL11.GL_QUADS);
