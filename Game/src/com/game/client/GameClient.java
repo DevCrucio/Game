d81907725eb2049a545378b639102fe88494358d
Jonas <Jonas@VAIO-Jonas>
2015-05-24 13:47:38 +0200

    Client and Server Start

---
+++ Game/src/com/game/client/GameClient.java
@@ -0,0 +1,11 @@
+package com.game.client;
+
+public class GameClient {
+	/*
+	 * Start Method
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Game/src/com/game/server/GameServer.java
@@ -0,0 +1,10 @@
+package com.game.server;
+
+public class GameServer {
+	/*
+	 * Start Method
+	 */
+	public static void main(String[] args) {
+	}
+
+}
