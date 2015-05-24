976771f30b84f39f7327640a81bb7f046a2bc528
Jonas <Jonas@VAIO-Jonas>
2015-05-24 21:29:56 +0200

    Game Server Setup

--- Game/src/com/game/server/GameServer.java
+++ Game/src/com/game/server/GameServer.java
@@ -1,5 +1,10 @@
 package com.game.server;
 
+import java.io.IOException;
+
+import com.esotericsoftware.kryonet.Server;
+import com.game.util.Misc;
+
 public class GameServer {
 	/*
 	 * Start Method
@@ -8,7 +13,25 @@ public class GameServer {
 		new GameServer();
 	}
 
-	public GameServer() {
+	/*
+	 * Server
+	 */
+	public Server server;
+	public World world;
 
+	/*
+	 * Server Start
+	 */
+	public GameServer() {
+		try {
+			server = new Server();
+			server.addListener(new ServerListener(this));
+			Misc.reg(server.getKryo());
+			server.bind(12345, 12345);
+			server.start();
+			world = new World();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
