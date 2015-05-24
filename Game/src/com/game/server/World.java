fb4a958e67defd5f18d214f000afab810c254c93
Jonas <Jonas@VAIO-Jonas>
2015-05-24 21:32:55 +0200

    World Accept

--- Game/src/com/game/server/World.java
+++ Game/src/com/game/server/World.java
@@ -7,11 +7,22 @@ import com.game.packet.Accept;
 
 public class World {
 	/*
+	 * Generate ID
+	 */
+	private int next = 0;
+
+	public int getID() {
+		return next++;
+	}
+
+	/*
 	 * Login
 	 */
 	public void login(String name, Connection con) {
 		Accept accept = new Accept();
 		accept.name = name;
+		accept.ID = getID();
+		con.sendTCP(accept);
 	}
 
 	/*
