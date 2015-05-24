4cb69ba7126b577186512c95a1432071e50532ae
Jonas <Jonas@VAIO-Jonas>
2015-05-24 15:39:12 +0200

    Test Game

---
+++ Game/src/com/game/client/GuiGame.java
@@ -0,0 +1,21 @@
+package com.game.client;
+
+public class GuiGame extends Gui {
+
+	public GuiGame(GameClient gc, String string, int i, String string2) {
+		super(gc);
+	}
+
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
