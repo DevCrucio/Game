81754cfce86555bde2a296384206dcd65724649b
Jonas <Jonas@VAIO-Jonas>
2015-05-24 20:43:14 +0200

    First Packets

---
+++ Game/src/com/game/packet/Accept.java
@@ -0,0 +1,6 @@
+package com.game.packet;
+
+public class Accept {
+	public String name;
+	public int ID;
+}
---
+++ Game/src/com/game/packet/Login.java
@@ -0,0 +1,5 @@
+package com.game.packet;
+
+public class Login {
+	public String name, pass;
+}
