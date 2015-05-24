b1c36afeae580a5e1d5513cf3ea4d7db0f286e18
Jonas <Jonas@VAIO-Jonas>
2015-05-24 18:15:07 +0200

    Entity Base

--- Game/src/com/game/client/Entity.java
+++ Game/src/com/game/client/Entity.java
@@ -3,7 +3,7 @@ package com.game.client;
 public abstract class Entity {
 	public final int ID;
 	public final World world;
-	public float x, y;
+	public float x, y, dx, dy;
 
 	public Entity(World world, int ID) {
 		this.ID = ID;
