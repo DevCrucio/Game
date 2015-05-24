1fda7e68179b60e71b079f9f00e3d58df08030d8
Jonas <Jonas@VAIO-Jonas>
2015-05-24 15:41:26 +0200

    Test World

---
+++ Game/src/com/game/client/World.java
@@ -0,0 +1,17 @@
+package com.game.client;
+
+public class World {
+	public GuiGame gg;
+
+	public World(GuiGame gg) {
+		this.gg = gg;
+	}
+
+	public void update(float delta) {
+
+	}
+
+	public void render() {
+
+	}
+}
