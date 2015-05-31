e02ab827bd1e5b7f0144e56fbf5273ccf250fb05
Jonas <Jonas@VAIO-Jonas>
2015-05-31 21:15:45 +0200

    Chunk rendering implemented (Intermediate Mode)

--- Game/src/com/game/client/Chunk.java
+++ Game/src/com/game/client/Chunk.java
@@ -19,6 +19,10 @@ public class Chunk {
 		gen = true;
 	}
 
+	public int getBlock(int x, int y) {
+		return block[x + 16 * y];
+	}
+
 	// update
 	public void update(float delta) {
 
@@ -29,19 +33,61 @@ public class Chunk {
 		if (gen) {
 			gen();
 		}
-		GL11.glColor3f(1, 0, 0);
 		GL11.glTranslatef(x * 256, y * 256, 0);
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glVertex2f(0, 0);
-		GL11.glVertex2f(256, 0);
-		GL11.glVertex2f(256, 256);
-		GL11.glVertex2f(0, 256);
-		GL11.glEnd();
+		for (int x = 0; x < 16; x++) {
+			for (int y = 0; y < 16; y++) {
+				GL11.glPushMatrix();
+				GL11.glTranslatef(x * 16, y * 16, 0);
+				int value = getBlock(x, y);
+				if (value == 1) {
+					GL11.glBegin(GL11.GL_QUADS);
+					GL11.glTexCoord2f(0f / 32f, 1f / 32f);
+					GL11.glVertex2f(0, 0);
+					GL11.glTexCoord2f(1f / 32f, 1f / 32f);
+					GL11.glVertex2f(16, 0);
+					GL11.glTexCoord2f(1f / 32f, 0f / 32f);
+					GL11.glVertex2f(16, 16);
+					GL11.glTexCoord2f(0f / 32f, 0f / 32f);
+					GL11.glVertex2f(0, 16);
+					GL11.glEnd();
+				}
+				if (value == 2) {
+					GL11.glBegin(GL11.GL_QUADS);
+					GL11.glTexCoord2f(1f / 32f, 1f / 32f);
+					GL11.glVertex2f(0, 0);
+					GL11.glTexCoord2f(2f / 32f, 1f / 32f);
+					GL11.glVertex2f(16, 0);
+					GL11.glTexCoord2f(2f / 32f, 0f / 32f);
+					GL11.glVertex2f(16, 16);
+					GL11.glTexCoord2f(1f / 32f, 0f / 32f);
+					GL11.glVertex2f(0, 16);
+					GL11.glEnd();
+				}
+				if (value == 3) {
+					GL11.glBegin(GL11.GL_QUADS);
+					GL11.glTexCoord2f(2f / 32f, 1f / 32f);
+					GL11.glVertex2f(0, 0);
+					GL11.glTexCoord2f(3f / 32f, 1f / 32f);
+					GL11.glVertex2f(16, 0);
+					GL11.glTexCoord2f(3f / 32f, 0f / 32f);
+					GL11.glVertex2f(16, 16);
+					GL11.glTexCoord2f(2f / 32f, 0f / 32f);
+					GL11.glVertex2f(0, 16);
+					GL11.glEnd();
+				}
+				GL11.glPopMatrix();
+			}
+		}
 	}
 
 	// generate VBO and Collision Box
 	public void gen() {
 		gen = false;
 		// TODO generate mesh
+	}
+
+	// Destroy model
+	public void destroy() {
+
 	}
 }
