1b154b17cc21dd6063a2d03af058a305033b2d18
Jonas <Jonas@VAIO-Jonas>
2015-06-09 22:08:53 +0200

    Added Chunk Modify

--- Game/src/com/game/client/ClientListener.java
+++ Game/src/com/game/client/ClientListener.java
@@ -5,9 +5,12 @@ import com.esotericsoftware.kryonet.Listener;
 import com.game.item.Item;
 import com.game.packet.Accept;
 import com.game.packet.ChunkAdd;
+import com.game.packet.ChunkChange;
 import com.game.packet.ChunkRem;
+import com.game.packet.EntityItemAdd;
 import com.game.packet.EntityMove;
 import com.game.packet.EntityRem;
+import com.game.packet.ItemEquip;
 import com.game.packet.ItemSend;
 import com.game.packet.PlayerAdd;
 import com.game.util.Misc;
@@ -53,15 +56,19 @@ public class ClientListener extends Listener {
 				ep.shoe = ap.shoe;
 				gg.world.entities.put(ap.ID, ep);
 			} else if (obj instanceof EntityMove) {
-				EntityMove em = (EntityMove) obj;
-				Entity entity = gg.world.entities.get(em.ID);
-				con.updateReturnTripTime();
-				float ping = con.getReturnTripTime();
-				entity.lerpX = em.x + em.dx * ping;
-				entity.lerpY = em.y + em.dy * ping;
-				entity.dx = em.dx;
-				entity.dy = em.dy;
-				entity.lookLeft = em.lookLeft;
+				try {
+					EntityMove em = (EntityMove) obj;
+					Entity entity = gg.world.entities.get(em.ID);
+					con.updateReturnTripTime();
+					float ping = con.getReturnTripTime();
+					entity.lerpX = em.x + em.dx * ping;
+					entity.lerpY = em.y + em.dy * ping;
+					entity.dx = em.dx;
+					entity.dy = em.dy;
+					entity.lookLeft = em.lookLeft;
+				} catch (NullPointerException e) {
+
+				}
 			} else if (obj instanceof EntityRem) {
 				EntityRem rem = (EntityRem) obj;
 				gg.world.entities.remove(rem.ID);
@@ -82,13 +89,48 @@ public class ClientListener extends Listener {
 								.clone();
 						gg.world.player.inv.items[i].name = si.name[i];
 						gg.world.player.inv.items[i].meta = si.meta[i];
+						gg.world.player.inv.items[i].rarity = si.rarity[i];
 					} else {
 						gg.world.player.inv.items[i] = null;
 					}
 				}
+			} else if (obj instanceof ItemEquip) {
+				ItemEquip ie = (ItemEquip) obj;
+				Entity e = gg.world.entities.get(ie.ID);
+				if (e instanceof EntityPlayer) {
+					EntityPlayer ep = (EntityPlayer) e;
+					for (int i = 0; i < 6; i++) {
+						if (ie.items[i] != -1) {
+							ep.inv.items[8 + i] = Item.items[ie.items[i]]
+									.clone();
+							ep.inv.amount[8 + i] = ie.amount[i];
+							if (ep.inv.items[8 + i] != null) {
+								ep.inv.items[8 + i].name = ie.name[i];
+								ep.inv.items[8 + i].meta = ie.meta[i];
+								ep.inv.items[8 + i].rarity = ie.rarity[i];
+							}
+						}
+					}
+				}
+			} else if (obj instanceof EntityItemAdd) {
+				EntityItemAdd eia = (EntityItemAdd) obj;
+				EntityItem ei = new EntityItem(gg.world, eia.ID);
+				ei.x = eia.x;
+				ei.y = eia.y;
+				ei.lerpX = eia.x;
+				ei.lerpY = eia.y;
+				ei.ItemID = eia.ItemID;
+				gg.world.entities.put(ei.ID, ei);
+			} else if (obj instanceof ChunkChange) {
+				ChunkChange cc = (ChunkChange) obj;
+				if (gg.world.chunks.containsKey(cc.cx + "x" + cc.cy)) {
+					Chunk chunk = gg.world.chunks.get(cc.cx + "x" + cc.cy);
+					chunk.setBlock(cc.x, cc.y, cc.id);
+				}
 			}
 		} catch (NullPointerException e) {
-			Misc.log("NullPointerException - Error");
+			Misc.log(e.getMessage());
+			e.printStackTrace();
 		}
 
 	}
