ef3eb9624dd21b294a368d457d1ff8e92bbcbde3
Jonas <Jonas@VAIO-Jonas>
2015-06-09 22:10:59 +0200

    Classes registered

--- Game/src/com/game/util/Misc.java
+++ Game/src/com/game/util/Misc.java
@@ -6,14 +6,19 @@ import java.util.Date;
 import com.esotericsoftware.kryo.Kryo;
 import com.game.packet.Accept;
 import com.game.packet.ChunkAdd;
+import com.game.packet.ChunkChange;
 import com.game.packet.ChunkRem;
 import com.game.packet.Color;
+import com.game.packet.EntityItemAdd;
 import com.game.packet.EntityMove;
 import com.game.packet.EntityRem;
+import com.game.packet.ItemDrop;
+import com.game.packet.ItemEquip;
 import com.game.packet.ItemSend;
 import com.game.packet.ItemSwitch;
 import com.game.packet.Login;
 import com.game.packet.PlayerAdd;
+import com.game.packet.PlayerClick;
 
 public class Misc {
 	/*
@@ -36,13 +41,25 @@ public class Misc {
 		// Own Classes
 		kryo.register(Accept.class);
 		kryo.register(ChunkAdd.class);
+		kryo.register(ChunkChange.class);
 		kryo.register(ChunkRem.class);
 		kryo.register(Color.class);
+		kryo.register(EntityItemAdd.class);
 		kryo.register(EntityMove.class);
 		kryo.register(EntityRem.class);
+		kryo.register(ItemDrop.class);
+		kryo.register(ItemEquip.class);
 		kryo.register(ItemSend.class);
 		kryo.register(ItemSwitch.class);
 		kryo.register(Login.class);
 		kryo.register(PlayerAdd.class);
+		kryo.register(PlayerClick.class);
+	}
+
+	public static float mod(float value, float div) {
+		float out = value % div;
+		if (out < 0)
+			out += div;
+		return out;
 	}
 }
