d530ee1111489856958865ebf55ca8f1aa3a4786
Jonas <Jonas@VAIO-Jonas>
2015-06-09 22:08:40 +0200

    Replaced Mouse

--- Game/src/com/game/client/GameClient.java
+++ Game/src/com/game/client/GameClient.java
@@ -1,5 +1,15 @@
 package com.game.client;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+
+import org.lwjgl.BufferUtils;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
@@ -8,6 +18,7 @@ import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 
 import com.game.util.Text;
+import com.game.util.Texture;
 
 public class GameClient {
 	/*
@@ -17,6 +28,8 @@ public class GameClient {
 	private long time;
 	private Gui gui;
 	public Text text;
+	public Texture mouse;
+	public int mouseX = 960 / 2, mouseY = 540 / 2;
 
 	/*
 	 * Start Method
@@ -47,6 +60,7 @@ public class GameClient {
 			Display.create();
 			Keyboard.create();
 			Mouse.create();
+			Mouse.setGrabbed(true);
 			// Init OpenGL
 			GL11.glMatrixMode(GL11.GL_PROJECTION);
 			GL11.glLoadIdentity();
@@ -60,6 +74,11 @@ public class GameClient {
 			// Init other Stuff
 			run = true;
 			text = new Text();
+			try {
+				mouse = new Texture("./src/Mouse.png");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			gui = new GuiIntro(this);
 			gui.create();
 			// First Delta timestamp
@@ -78,6 +97,17 @@ public class GameClient {
 			float delta = getDelta();
 			if (delta <= 100)
 				gui.update(delta);
+			// Mouse update
+			mouseX += Mouse.getDX();
+			if (mouseX < 0)
+				mouseX = 0;
+			if (mouseX > 960)
+				mouseX = 960;
+			mouseY += Mouse.getDY();
+			if (mouseY < 0)
+				mouseY = 0;
+			if (mouseY > 540)
+				mouseY = 540;
 			// Clear Screen
 			GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT | GL11.GL_COLOR_BUFFER_BIT
 					| GL11.GL_STENCIL_BUFFER_BIT);
@@ -85,17 +115,34 @@ public class GameClient {
 			GL11.glPushMatrix();
 			gui.render();
 			GL11.glPopMatrix();
+			GL11.glPushMatrix();
+			GL11.glColor4f(1, 1, 1, 1);
+			mouse.bind();
+			GL11.glTranslatef(mouseX, mouseY, 0);
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glTexCoord2f(0, 1);
+			GL11.glVertex2f(-8, -8);
+			GL11.glTexCoord2f(1, 1);
+			GL11.glVertex2f(8, -8);
+			GL11.glTexCoord2f(1, 0);
+			GL11.glVertex2f(8, 8);
+			GL11.glTexCoord2f(0, 0);
+			GL11.glVertex2f(-8, 8);
+			GL11.glEnd();
+			GL11.glPopMatrix();
 			while (Mouse.next()) {
 			}
 			while (Keyboard.next()) {
 			} // Clear Buffers
-			// Show on Screen
+				// Show on Screen
 			Display.update();
 			// Cap FPS
 			Display.sync(60);
 			// Close
 			if (Display.isCloseRequested())
 				run = false;
+			// Screenshot
+			screen();
 		}
 	}
 
@@ -134,5 +181,54 @@ public class GameClient {
 		this.gui.destroy();
 		this.gui = gui;
 		this.gui.create();
+	}
+
+	/*
+	 * Screenshot Method
+	 */
+	boolean down = false;
+
+	public void screen() {
+		if (!down) {
+			if (Keyboard.isKeyDown(Keyboard.KEY_F12)) {
+				down = true;
+				GL11.glReadBuffer(GL11.GL_FRONT);
+				int width = Display.getDisplayMode().getWidth();
+				int height = Display.getDisplayMode().getHeight();
+				int bpp = 4; // Assuming a 32-bit display with a byte each for red, green, blue, and alpha.
+				ByteBuffer buffer = BufferUtils.createByteBuffer(width * height
+						* bpp);
+				GL11.glReadPixels(0, 0, width, height, GL11.GL_RGBA,
+						GL11.GL_UNSIGNED_BYTE, buffer);
+
+				Date date = new Date();
+				SimpleDateFormat dateFormat = new SimpleDateFormat(
+						"yyyy-MM-dd HH-mm-ss");
+				File file = new File("./" + dateFormat.format(date) + ".png"); // The file to save to.
+				String format = "PNG"; // Example: "PNG" or "JPG"
+				BufferedImage image = new BufferedImage(width, height,
+						BufferedImage.TYPE_INT_RGB);
+				for (int x = 0; x < width; x++) {
+					for (int y = 0; y < height; y++) {
+						int i = (x + (width * y)) * bpp;
+						int r = buffer.get(i) & 0xFF;
+						int g = buffer.get(i + 1) & 0xFF;
+						int b = buffer.get(i + 2) & 0xFF;
+						image.setRGB(x, height - (y + 1), (0xFF << 24)
+								| (r << 16) | (g << 8) | b);
+					}
+				}
+
+				try {
+					ImageIO.write(image, format, file);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		} else {
+			if (!Keyboard.isKeyDown(Keyboard.KEY_F12)) {
+				down = false;
+			}
+		}
 	}
 }
